419f8eb313eb2e3704a5da97966b69cdf4e45afd
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 15:21:37 +0200

    la classe String

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -1,12 +1,17 @@
 package cours1;
 
 public class HelloWorld {
+	
 	// Déclaration de la première variable
+	// Remplacer les prints par la classe String
+	
 	public static void main(String[] args) {
-		System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:");
+		String premiereNouvelle ="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:";
+		System.out.println(premiereNouvelle);
 		int nbrePlanetes = 9;
 		System.out.println(nbrePlanetes);
-		System.out.println("Il y a quelques années cependant,elles étaient au nombre de :");
+		String deuxiemeNouvelle = "Il y a quelques années cependant,elles étaient au nombre de :";
+		System.out.println(deuxiemeNouvelle);
 		nbrePlanetes = 8;
 		System.out.println(nbrePlanetes);
 	}
