32160e25d6b85f674e4753db8d14c038c1e2dd22
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 15:18:18 +0200

    première variable déclarée

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -1,9 +1,14 @@
 package cours1;
 
 public class HelloWorld {
-
+	// Déclaration de la première variable
 	public static void main(String[] args) {
-		System.out.println("Hello Universe!!!");
+		System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:");
+		int nbrePlanetes = 9;
+		System.out.println(nbrePlanetes);
+		System.out.println("Il y a quelques années cependant,elles étaient au nombre de :");
+		nbrePlanetes = 8;
+		System.out.println(nbrePlanetes);
 	}
 
 }
