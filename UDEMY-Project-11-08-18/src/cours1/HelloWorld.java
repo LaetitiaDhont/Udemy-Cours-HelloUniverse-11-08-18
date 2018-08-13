a26e3b41a97df9501b17202a1427b9aea3da1d68
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 16:43:44 +0200

    switch case qui remplace les else if else

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -1,35 +1,32 @@
 package cours1;
 
 public class HelloWorld {
-	
+
 	// Déclaration de la première variable
 	// Remplacer les prints par la classe String
 	// Ajouter les opérateurs d'incrémentation
 	// Utilisation de la concaténation
 	// Première instruction conditionnelle
 	// Ajout du else
-	// Ajout du else if 
-	
+	// Ajout du else if
+	// Remplacer les else / else if / else par un switch case default break
+
 	public static void main(String[] args) {
-		
+
 		int nbrePlanetes = 8;
-		
-		if (nbrePlanetes == 8) {
-			System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:" + nbrePlanetes);
+
+		switch (nbrePlanetes) {
+		case 8:
+			System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de 8");
+			break;
+		case 9:
+			System.out.println("Il y a quelques années cependant,elles étaient au nombre de : 9");
+			break;
+		default:
+			System.out.println("Au cours de l'ère moderne, le nombre de planètes n'a jamais été officiellement de : 44");
 
 		}
-		
-		else if (nbrePlanetes != 8){
-			System.out.println("Au cours de l'ère moderne, le nombre de planètes n'a jamais été officiellement de : 44");
-		}
-		
-		else {
-			
-		System.out.println("Il y a quelques années cependant,elles étaient au nombre de :");
-		nbrePlanetes++;
-		
-		}
-		
+
 	}
 
 }
