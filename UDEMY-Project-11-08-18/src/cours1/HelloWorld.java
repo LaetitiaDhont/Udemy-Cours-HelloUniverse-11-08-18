f8b5f0e4cc97444cfbff0cdcc675e4a51bad9e61
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 15:54:33 +0200

    utilisation du else if

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -8,19 +8,28 @@ public class HelloWorld {
 	// Utilisation de la concaténation
 	// Première instruction conditionnelle
 	// Ajout du else
+	// Ajout du else if 
 	
 	public static void main(String[] args) {
-		String premiereNouvelle ="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:";
+		
 		int nbrePlanetes = 8;
+		
 		if (nbrePlanetes == 8) {
-		System.out.println(premiereNouvelle + nbrePlanetes);
+			System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:" + nbrePlanetes);
+
+		}
+		
+		else if (nbrePlanetes != 8){
+			System.out.println("Au cours de l'ère moderne, le nombre de planètes n'a jamais été officiellement de : 44");
 		}
 		
 		else {
-		String deuxiemeNouvelle = "Il y a quelques années cependant,elles étaient au nombre de :";
+			
+		System.out.println("Il y a quelques années cependant,elles étaient au nombre de :");
 		nbrePlanetes++;
-		System.out.println(deuxiemeNouvelle + nbrePlanetes);
+		
 		}
+		
 	}
 
 }
