d6b3af34ce89f5f7a5cfe14f9a9bb59a574dda52
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 15:38:56 +0200

    première instruction conditionnelle

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -6,15 +6,18 @@ public class HelloWorld {
 	// Remplacer les prints par la classe String
 	// Ajouter les opérateurs d'incrémentation
 	// Utilisation de la concaténation
+	// Première instruction conditionnelle
 	
 	public static void main(String[] args) {
 		String premiereNouvelle ="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:";
 		int nbrePlanetes = 8;
+		if (nbrePlanetes == 8) {
 		System.out.println(premiereNouvelle + nbrePlanetes);
+		}
+		
 		String deuxiemeNouvelle = "Il y a quelques années cependant,elles étaient au nombre de :";
 		nbrePlanetes++;
 		System.out.println(deuxiemeNouvelle + nbrePlanetes);
-
 	}
 
 }
