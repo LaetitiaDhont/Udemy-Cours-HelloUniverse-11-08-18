fe191d549ee058e0e9f821b50bb6c8ff2e1a0f61
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 15:27:55 +0200

    concaténation

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -5,16 +5,16 @@ public class HelloWorld {
 	// Déclaration de la première variable
 	// Remplacer les prints par la classe String
 	// Ajouter les opérateurs d'incrémentation
+	// Utilisation de la concaténation
 	
 	public static void main(String[] args) {
 		String premiereNouvelle ="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de:";
-		System.out.println(premiereNouvelle);
 		int nbrePlanetes = 8;
-		System.out.println(nbrePlanetes);
+		System.out.println(premiereNouvelle + nbrePlanetes);
 		String deuxiemeNouvelle = "Il y a quelques années cependant,elles étaient au nombre de :";
-		System.out.println(deuxiemeNouvelle);
 		nbrePlanetes++;
-		System.out.println(nbrePlanetes);
+		System.out.println(deuxiemeNouvelle + nbrePlanetes);
+
 	}
 
 }
