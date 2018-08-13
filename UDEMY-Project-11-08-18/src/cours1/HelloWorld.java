3f46bc2b08586bda33e2ea1a570375de7d81fdb8
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-13 17:53:25 +0200

    la boucle while

--- UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
+++ UDEMY-Project-11-08-18/src/cours1/HelloWorld.java
@@ -12,19 +12,27 @@ public class HelloWorld {
 	// Remplacer les else / else if / else par un switch case default break
 	// Utiliser la structure itérative avec for
 	// Utiliser la structure for avec switch
+	// Remplacer la boucle for par une boucle while
 
 	public static void main(String[] args) {
-		for (int nbrePlanetes = 8; nbrePlanetes < 11; nbrePlanetes++) {
+
+		int nbrePlanetes = 8;
+
+		while (nbrePlanetes < 11) {
 			switch (nbrePlanetes) {
 			case 8:
 				System.out.println("Auparavant le nombre de planètes était de : " + nbrePlanetes);
 				break;
 			case 9:
-				System.out.println("Actuellement le nombre de planètes est de : " + nbrePlanetes);
+				System.out.println("Le nombre de planètes actuel est de : " + nbrePlanetes);
 				break;
-			case 10:
-				System.out.println("Le nombre n'a cependant jamais atteint : " + nbrePlanetes);
+			default:
+				System.out.println("Le nombre n'a cependant jamais été : " + nbrePlanetes);
 			}
+			
+			nbrePlanetes++;
 		}
+
 	}
+
 }
